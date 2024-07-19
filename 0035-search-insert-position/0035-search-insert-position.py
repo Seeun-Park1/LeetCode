c505781c165cb043d150fdb061dcf977566d5e1f
Saeeun Park <seeun.park22@gmail.com>
2024-07-19 11:40:23 +0900

    Time: 33 ms (26.68%), Space: 12.4 MB (13.23%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        nums.append(target)
+        nums.sort()
+
+        for i, j in enumerate(nums): 
+            if target == j:
+                return i 
