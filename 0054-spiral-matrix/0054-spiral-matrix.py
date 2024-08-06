404ebe956770dd5bdb26381f9613cd8b2ae82f48
Saeeun Park <seeun.park22@gmail.com>
2024-08-06 11:59:23 +0900

    Time: 24 ms (7.42%), Space: 11.6 MB (32.8%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,3 +1,6 @@
 class Solution(object):
     def spiralOrder(self, matrix):
         return matrix and list(matrix.pop(0)) + self.spiralOrder( zip(*matrix)[::-1])
+
+    def turnRight(self, vector): 
+        return (vector[1], -1 * vector[0])
