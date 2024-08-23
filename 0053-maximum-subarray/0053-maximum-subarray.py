93dc1698951fef3de7d6f90e803150b05e7eaada
Saeeun Park <seeun.park22@gmail.com>
2024-08-24 06:32:02 +0900

    Time: 578 ms (6.27%), Space: 24.3 MB (97.42%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,8 +3,6 @@ class Solution(object):
         arr = []
         arr.append(nums[0])
         maxSum = arr[0]
-
-
         for i in range(1, len(nums)): 
             arr.append(max(arr[i-1] + nums[i], nums[i]))
 
